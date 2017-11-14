929a17db6157e3698a7552432ff0d312887c2f4c
Nick Baidikoff <nbaidikoff@gmail.com>
2017-11-15 00:10:03 +0200

    Init project

---
+++ mobile/Places To Visit/Places To Visit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Places To Visit
+//
+//  Created by Nick Baidikoff on 11/15/17.
+//  Copyright © 2017 Nick Baidikoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
