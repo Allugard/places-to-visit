b1fedca471715c590b4fa3a39cfc3cb379de498d
Allugard <cen4ukk@google.com>
2017-12-26 18:38:03 +0200

    update place query

--- web/src/main/java/yo/antihype/team/repository/PlaceRepository.java
+++ web/src/main/java/yo/antihype/team/repository/PlaceRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 public interface PlaceRepository extends CrudRepository<Place, Long> {
 
-    @Query(value = "SELECT * FROM place JOIN user_place ON place.id = user_place.place_id JOIN users ON user_place.user_id = users.id" +
-            "  WHERE users.username  = :username", nativeQuery = true)
+    @Query(value = "SELECT * FROM PLACE JOIN USERS_PLACE ON PLACE.ID = USERS_PLACE.PLACE_ID JOIN USERS ON USERS_PLACE.USERS_ID = USERS.ID" +
+            "  WHERE USERS.USERNAME = :username", nativeQuery = true)
     List<Place> findPlacesByUsername(@Param("username") String username);
     Place findByLatAndLng(String lat, String lng);
 
